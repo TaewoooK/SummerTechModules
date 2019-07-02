86775e8506995975b696da1589d56182e08b1009
SummerTech <summertech1@example.com>
2019-07-02 09:24:23 -0400

    TicTacTow Finished

--- TicTacToe.py
+++ TicTacToe.py
@@ -24,21 +24,61 @@ def placeMove(board, numSpace, turn):
     board[numSpace] = turn
     return board
 
+def switchTurn(turnNum):
+    if turnNum%2==0:
+        return "X"
+    else:
+        return "O"
+
+def checkCol(board, turn):
+    if board[1] == turn and board[4] == turn and board[7] == turn:
+        return True
+    elif board[2] == turn and board[5] == turn and board[8] == turn:
+        return True
+    elif board[3] == turn and board[6] == turn and board[9] == turn:
+        return True
+    else:
+        return False
+
+def checkRow(board, turn):
+    if board[1] == turn and board[2] == turn and board[3] == turn:
+        return True
+    elif board[4] == turn and board[5] == turn and board[6] == turn:
+        return True
+    elif board[7] == turn and board[8] == turn and board[9] == turn:
+        return True
+    else:
+        return False
+
 board = []
-good = False
+turnNum = 0
+win = False
 turn = "X"
+
 print("Welcome to TicTacToe")
+
 setBoardDefault(board)
 drawBoard(board)
 
-while good == False:
-    numSpace = playerMove()
-    good = checkMove(numSpace, board)
+while win == False:
+    print("Player", turn, end="")
+    print("'s turn")
 
-board = placeMove(board, numSpace, turn)
-drawBoard(board)
+    goodMove = False
 
+    while goodMove == False:
+        numSpace = playerMove()
+        goodMove = checkMove(numSpace, board)
 
+    board = placeMove(board, numSpace, turn)
+    drawBoard(board)
 
+    win = checkCol(board, turn)
+    if win == False:
+        win = checkRow(board, turn)
 
+    turnNum+=1
+    turn = switchTurn(turnNum)
 
+turn = switchTurn(turnNum-1)
+print("PLAYER", turn, "WINS!")
