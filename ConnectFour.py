ca2e92be1dd60a1b9b76f800851d34f030d0426b
SummerTech <summertech1@example.com>
2019-07-02 11:11:38 -0400

    connectfour

--- ConnectFour.py
+++ ConnectFour.py
@@ -34,23 +34,50 @@ def switchTurn(turnNum):
     else:
        return "O"
 
+def winHor(board):
+    for i in range(0, 6):
+        for j in range (0, 4):
+            if board[i][j] == turn and board[i][j+1] == turn and board[i][j+2] == turn and board[i][j+3] == turn:
+                return True
+    return False
+
+def winVert(board):
+    for i in range(0, 7):
+        for j in range(0, 4):
+            if board[j][i] == turn and board[j+1][i] == turn and board[j+2][i] == turn and board[j+3][i] == turn:
+                return True
+    return False
+
 turn = "X"
 turnNum = 0
+win = False
 print("Welcome to Connect Four!")
 showBoard(board)
 
-while True:
+while win == False:
     print("Player", turn, "goes first")
 
     goodCol = False
     goodRow = False
-    while goodCol == False or goodRow == False:
+    while goodCol == False:
         colSpace = playerMoveCol()
-        goodRow = checkRow(colSpace, turn)
         goodCol = checkCol(colSpace)
-
+        if goodCol == True:
+            goodRow = checkRow(colSpace, turn)
+        if goodRow == False:
+            goodCol = False
+        
     board = placeMove(board, colSpace, turn)
     showBoard(board)
 
+    winHor = winHor(board)
+    winVert = winVert(board)
+
+    if winHor or winVert:
+        print("WIN!")
+        win = True
+
     turnNum += 1
     turn = switchTurn(turnNum)
+
+
